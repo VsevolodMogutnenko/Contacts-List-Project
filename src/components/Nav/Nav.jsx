93671c24c1942c7016eb457da3536f950c35f5e7
cracking-fad097 <crackingfad@gmail.com>
2026-01-17 11:15:58 +0100

    Update Register

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
 import { AusNav } from '../AusNav/AusNav'
 import logo from '../../assets/logo.png'
+import styles from './Nav.module.css'
 
 export const Nav = () => {
     return (
-        <ul>
-            <li><Link to="/"><img src={logo} alt="logo" width={100}/></Link></li>
+        <ul className={styles.navList}>
+            <li><Link to="/"><img src={logo} alt="logo" width={100} height={100}/></Link></li>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/PhoneBook">PhoneBook</Link></li>
             <AusNav />
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,28 +1,47 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { FaEye } from "react-icons/fa";
+import { LuEyeClosed } from "react-icons/lu";
 
 export const Register = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [type, setType] = useState(false)
 
     const handleInputChange = (event) => {
-      console.log(event.target.placeholder)
-      // switch(event) {
-      //   case name:
-      //     console.log('name')
-      //     break
-      //   case email:
-      //     console.log('email')
-      //     break
-      // }
+        const {value, placeholder} = event.target
+        switch(placeholder) {
+            case 'Name':
+                setName(value)
+                break
+            case 'Email':
+                setEmail(value)
+                break
+            case 'Password':
+                setPassword(value)
+                break
+            default:
+                break
+        }
+    }
+
+    const handleTypeChange = () => {
+        if(type === false) {
+            setType(true)
+        } else {
+            setType(false)
+        }
     }
 
     return <>
         <form>
             <input type="text" placeholder="Name" value={name} onChange={handleInputChange} />
             <input type="email" placeholder="Email" value={email} onChange={handleInputChange}/>
-            <input type="password" placeholder="Password" value={password} onChange={handleInputChange}/>
+            <div>
+                <input type={type?"text":"password"} placeholder="Password" value={password} onChange={handleInputChange}/>
+                <button type="button" onClick={handleTypeChange}>{type?<FaEye size={20}/>:<LuEyeClosed size={20}/>}</button>
+            </div>
             <button type="submit">Register</button>
         </form>
     </>
